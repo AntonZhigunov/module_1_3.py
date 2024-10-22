1c6bf07ab002dc4284c748fb570382fbb20fe86d
AntonZhigunov <azhigunov79@gmail.com>
2024-10-22 17:13:14 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name="Anton"
+print('Имя:',name)
+age = 45
+print("Возраст:",age,"лет")
+new_age = 3
+print("Возраст в 2027 году:",age+new_age,"лет")
+is_student = True
+print("Я студент",is_student)
